ee16e83324d5db05a28d253fb0121ff2d694a08e
Dru01 <batmanrpp@hotmail.com>
2023-05-30 13:40:26 -0600

    Intialize Xamarin project

---
+++ GasolinerasVIP/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GasolinerasVIP.Models;
+using GasolinerasVIP.ViewModels;
+using GasolinerasVIP.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GasolinerasVIP.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
