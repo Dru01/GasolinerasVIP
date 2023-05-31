db8cd25f89a44ea122292111f0bacc9e1f13a393
Dru01 <batmanrpp@hotmail.com>
2023-05-30 19:32:21 -0600

    Intialize Data Base server

---
+++ GasolinerasVIP.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GasolinerasVIP.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiaDCEA.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Test> registroTest { get; set; }
+    }
+}
