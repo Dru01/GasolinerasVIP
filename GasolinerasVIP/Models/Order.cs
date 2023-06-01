9e5b918c9698c054df5a2345cbc90cc0f1345c8a
5an7y <jose.santiago7@hotmail.com>
2023-06-01 00:08:51 -0600

    Order Object

---
+++ GasolinerasVIP/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GasolinerasVIP.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public GasStation Station { get; set; }
+        public double OrderedMagna { get; set; }
+        public double OrderedPremium { get; set; }
+        private DateTime _transactionTime;
+        public DateTime TransactionTime
+        {
+            get => _transactionTime;
+            set { _transactionTime = DateTime.Now; }
+        }
+        public int state { get; set; } // 1 = En la Estacion, 2 = En Camino, 3 = Pedido Realizado
+    }
+}
