563074f0f2005e9ec661ea06c72cc4f1076c591d
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-01 23:14:28 -0600

    GasStation and Transactions API

---
+++ GasolinerasVIP.API/Controllers/TransactionController.cs
@@ -0,0 +1,68 @@
+﻿using GasolinerasVIP.API.Models;
+using GuiaDCEA.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;    
+
+namespace GasolinerasVIP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> PostTransaction(Transaction transaction)
+        {
+            context.Add(transaction);
+            await context.SaveChangesAsync();
+            return transaction.Id;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaction>> GetTransactionById(int id)
+        {
+            var transaction =  await context.Transaction.FindAsync(id);
+            if(transaction == null) { return NotFound(); }
+            return transaction;
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return context.Transaction.Any(e => e.Id == id);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutTransaction(int id, Transaction transaction)
+        {
+            if (id != transaction.Id)
+                return BadRequest();
+
+            context.Entry(transaction).State = EntityState.Modified;
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+    }
+}
--- GasolinerasVIP.API/Models/Transaction.cs
+++ GasolinerasVIP.API/Models/Transaction.cs
@@ -1,18 +1,21 @@
 ﻿namespace GasolinerasVIP.API.Models
 {
+    enum Status
+    {
+        RECEIVED, 
+        IN_PROGRESS,
+        COMPLETED
+    }
     public class Transaction
     {
         public int Id { get; set; }
-
-        // Needed GasStation
         // Needed UserID
-        // Needed GasPipe?
-        // Needed Address
-
+        public GasStation GasStation { get; set; }
+        public string Address { get; set; } 
         public DateTime ReceivedOrderDate { get; set; }
         public DateTime DeliveredOrderDate { get; set; }
-        public int Status { get; set; } // ENUM? Recibido, en camino, completado
-        public int GasType { get; set; } // ENUM? Magna, Premium, Diesel
+        public int Status { get; set; } // ENUM {RECEIVED, IN_PROGRESS, COMPLETED}
+        public int GasType { get; set; } // ENUM {MAGNA PREMIUM, DIESEL}
         public decimal GasPrice { get; set; }
         public decimal Liters { get; set; }
         public decimal Subtotal { get; set; }
@@ -21,7 +24,6 @@
         public decimal DeliveryFee { get; set; }
         public decimal Disccount { get; set; }
         public decimal Total { get; set; }
-
         // Needed Payment method
     }
 }
