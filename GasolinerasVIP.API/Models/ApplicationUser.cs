5d7cf9c407eeab4154dc57ce6c8bab05bd90b3db
Dru01 <batmanrpp@hotmail.com>
2023-06-08 15:14:14 -0600

    User interface

---
+++ GasolinerasVIP.API/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace GasolinerasVIP.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
