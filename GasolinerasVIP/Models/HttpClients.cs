a0fb61a3a863ef6a8141c79044cc332a7636c019
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-07 18:26:36 -0600

    Simple HttpClient to fetch GasStations

---
+++ GasolinerasVIP/Models/HttpClients.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasolinerasVIP.Models
+{
+   
+    public class Client
+    {
+        private static HttpClient sharedClient = new HttpClient()
+        { 
+            BaseAddress = new Uri("https://gasvip.azurewebsites.net/api/GasStation"),
+        };
+        public static async Task<List<GasStation>> GetGasStations()
+        {
+            var response = await sharedClient.GetAsync("GasStation");
+            var jsonResponse = response.Content.ReadAsStringAsync().Result;
+            List<GasStation> gasStations = GasStation.FromJson(jsonResponse);
+            return gasStations;
+        }
+    }
+}
